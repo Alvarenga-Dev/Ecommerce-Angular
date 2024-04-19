0075c7fdeee849f7a6f32e9a5215f583387d3607
Lucas Alvarenga <lucasalvarenga020@gmail.com>
2024-04-19 15:24:10 -0300

    create init components

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss'
+})
+
+// first task - fetch from server
+export class HomeComponent {
+
+}
